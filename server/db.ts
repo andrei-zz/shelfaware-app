282fcac64aaf8289504fb084b13bf7d3ea86c0ce
Andrei <andreii@uw.edu>
2025-04-27 18:53:10 -0700

    fix .env not found

--- server/db.ts
+++ server/db.ts
@@ -4,9 +4,9 @@ import { config } from "dotenv";
 
 import * as schema from "~/../database/schema";
 
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is required");
-
 config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is required");
 
 const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle({
